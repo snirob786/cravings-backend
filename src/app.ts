d09345284bc1996c35cf1958de0cb132ddf5ddc2
Saifur Rahman Nirob <snirob786@gmail.com>
2024-07-13 12:20:10 +0600

    feat: blood group updated with successful super admin registration

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({ origin: ['http://localhost:5173'] }));
+app.use(cors({ origin: ['http://localhost:3000'] }));
 
 // application routes
 app.use('/api/v1', router);
